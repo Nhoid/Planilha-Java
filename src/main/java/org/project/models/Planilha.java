6675e219fdd1f9ad7321abbce7e58c851a47e626
dinhoPC <geraldinhofilho14@gmail.com>
2025-01-04 20:58:43 -0300

    comentando

--- src/main/java/org/project/models/Planilha.java
+++ src/main/java/org/project/models/Planilha.java
@@ -20,19 +20,28 @@ public class Planilha {
 
     public Planilha complete() {
 
+        // verifica se a planilha esta ordenada, necessario que esteja
         if (!this.ordenada)
             ordenar();
 
+        // pega a as coordenadas das ultima celula
         int maiorX = celulas.getLast().getX();
         int maiorY = celulas.getLast().getY();
 
         int index = 0;
 
+        // vai iterando sobre toda a matriz
         for (int y = 0; y < maiorY; y++) {
             for (int x = 0; x < maiorX; x++) {
 
                 Celula celula = celulas.get(index);
 
+                /*
+                *  Aqui ele faz uma verificacao se a celula atual esta na posicao que deveria
+                *  para ela estar na posicao certa, o x e y tem que ser o mesmo
+                *  se o x e y for diferente, significa que deveria existir uma ou mais celula antes dela
+                *  ele cria uma celula vazia
+                */
                 if ( !(celula.getX() == x && celula.getY() == y) ) {
                     celulas.add(index, new Celula(x, y));
                 }
